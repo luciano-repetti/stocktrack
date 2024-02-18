6026ff013218331afe680eb90117a80e9a2fad1a
luciano-repetti <lucianorepetti2020@gmail.com>
2024-02-18 19:19:26 -0300

    edit navbar expand

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,68 @@
-import Link from "next/link"
-import styles from "@/styles/modules/Nav.module.css"
-import Image from "next/image"
+import Link from 'next/link'
+import styles from '@/styles/modules/Nav.module.css'
+import Image from 'next/image'
+import { useState } from 'react'
 
 const Nav = () => {
 
-    const navigations = [{ icon: "house_home_icon", name: "FINANZAS" }, { icon: "cart_icon", name: "ORDEN DE COMPRA" }, { icon: "box_icon", name: "INVENTARIO" }, { icon: "history_icon", name: "HISTORIAL" }, { icon: "notes_icon", name: "NOTAS" }, { icon: "question_icon", name: "HELP" }]
+    const [expand, setExpand] = useState(false)
+
+    const navigations = [
+        { icon: 'house_home_icon', name: 'FINANZAS', link: 'finanzas' },
+        { icon: 'cart_icon', name: 'ORDEN DE COMPRA', link: 'orden_de_compra' },
+        { icon: 'box_icon', name: 'INVENTARIO', link: 'inventario' },
+        { icon: 'history_icon', name: 'HISTORIAL', link: 'historial' },
+        { icon: 'notes_icon', name: 'NOTAS', link: 'notas' },
+        { icon: 'question_icon', name: 'AYUDA', link: 'ayuda' },
+    ]
+
+    const handleExpandMenu = () => {
+        setExpand(!expand)
+    }
 
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header} ${expand ? styles.expand : ""}`}>
             <article>
-                <h1>STOCKTRACK</h1>
+                {!expand && (
+                    <h2>ST</h2>
+                )}
+                <h1 style={!expand ? { display: "none" } : {}}>STOCKTRACK</h1>
             </article>
+
             <nav className={styles.container}>
-                {navigations.map(({ name, icon }, index) => (
-                    <Link className={index == 0 ? styles.active : ""} href={""} key={crypto.randomUUID()}>
-                        <Image width={35} height={35} src={`/icons/${icon}.png`} alt={`Imagen de ${name}`} />
-                        {name}
-                    </Link>
+                {navigations.map((data, index) => (
+                    <LinkCustom expand={expand} data={data} active={index == 0} key={crypto.randomUUID()} />
                 ))}
             </nav>
+
+            <div className={`${styles.containerExpand} ${expand ? styles.expand : ""}`}>
+                <button onClick={handleExpandMenu} className={styles.buttonExpand} >
+                    <Image width={30} height={30} src={"/icons/arrow_rigth_icon.svg"} alt='flecha expande menu' />
+                </button>
+            </div>
         </header>
     )
 }
 
 export default Nav
+
+const LinkCustom = ({ data, active, expand }) => {
+    const { name, icon, link } = data
+
+    return (
+        <Link
+            className={`${expand ? styles.expand : ""} ${active ? styles.active : ""}`}
+            href={link}
+        >
+            <Image
+                width={35}
+                height={35}
+                src={`/icons/${icon}.png`}
+                alt={`Imagen de ${name}`}
+            />
+            {expand && (
+                name
+            )}
+        </Link>
+    )
+}
