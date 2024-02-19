be41b293cf5049526442b11b025604842573f43c
luciano-repetti <lucianorepetti2020@gmail.com>
2024-02-18 21:21:53 -0300

    fix styles navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,12 @@ import Nav from '@/components/Nav'
 
 export default function Home() {
   return (
-    <>
+    <main style={{ display: 'flex' }}>
       <Header />
       <Nav />
-    </>
+      <main style={{ marginTop: '8svh' }}>
+        <h3>Hola</h3>
+      </main>
+    </main>
   )
 }
